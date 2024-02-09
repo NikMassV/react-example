a59795b06221ba9774a3623784818657df5641b7
NikMassV <slipknoshka777@gmail.com>
2024-02-09 12:04:05 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Test from './Test'
+import Test1 from './Test1'
+import Header from './Header/Header'
+import Goods from './Goods'
+
+const headerData = {
+  site_name: 'my test site name',
+  nav: [
+    {"link": "nav1", "text": "my link1"},
+    {"link": "nav2", "text": "my link2"},
+    {"link": "nav3", "text": "my link3"},
+  ]
+}
+
+const goods = [
+  {"title": "grape", "cost": 330, "image": "image1"},
+  {"title": "pear", "cost": 200, "image": "image2"},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header data={headerData}/>
+      <Test />
+      <Test1 />
+      {goods.map(item => <Goods key={item.title} title={item.title} cost={item.cost} image={item.image} />)}
+    </>
+  )
 }
 
 export default App;
---
+++ src/Test.js
@@ -0,0 +1,12 @@
+import Test1 from './Test1'
+
+function Test() {
+    return (
+        <>
+        <Test1 />
+        <h1>Test component</h1>
+        </>
+    )
+}
+
+export default Test;
